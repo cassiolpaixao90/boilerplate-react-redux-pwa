c47cff4154e6bb0188d65d031bb1baf47c6b17d1
Cássio Paixão <cassio.paixao@accenture.com>
2018-06-21 10:16:41 -0300

    setting scafolding

---
+++ config/webpack.dev.js
@@ -0,0 +1,73 @@
+const path = require("path")
+const webpack = require("webpack")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: {
+    main: [
+      "babel-runtime/regenerator",
+      "webpack-hot-middleware/client?reload=true",
+      "./src/main.js"
+    ]
+  },
+  mode: "development",
+  output: {
+    filename: "[name]-bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+    publicPath: "/"
+  },
+  devServer: {
+    contentBase: "dist",
+    overlay: true,
+    stats: {
+      colors: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.jpg$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("development")
+      }
+    }),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      inject: true
+    })
+  ]
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+require("./main.css")
+require("./content.css")
+require("./images/link.jpg")
+console.log(`Environment is ${process.env.NODE_ENV}`)
